5bda482c4f83a6af1e1b3e6bade98123d9b2ecf2
beggart <b.eggart@gmail.com>
2021-05-11 11:01:56 +0200

    minor change in plot1.R and initial commit for plot2.R

--- plot2.R
+++ plot2.R
@@ -10,23 +10,20 @@ unzip(zipfile = "dataFiles.zip")
 SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
 NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
+# Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? 
+# Use the base plotting system to make a plot answering this question.
 
-
-# Prevents histogram from printing in scientific notation
 NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
 
-#total emissions by year
-totalNEI <- NEI[, lapply(.SD, sum, na.rm = TRUE), .SDcols = c("Emissions"), by = year]
+png(filename='plot2.png')
 
-png(filename='plot1.png')
-
-#plot
 barplot(totalNEI[, Emissions]
         , names = totalNEI[, year]
         , xlab = "time (year)",
         , ylab = expression("total PM"[2.5]*" emission (ton)"),
-        , main = "Emissions in the United States over the Years")
+        , main = "Emissions in Baltimore City, Maryland over the years")
 
 dev.off()
